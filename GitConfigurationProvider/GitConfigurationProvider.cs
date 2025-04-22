b43b6b774ecc26b1517fccbcc8d4a94a78a0c990
Christian Helmich <kagekirin@gmail.com>
2025-04-22 10:36:33 +0900

    feat: implement GitConfigurationProvider constructor taking LibGit2Sharp.Configuration (feat/provider-ctor-with-repo-config) [#7]
    - **feat: implement GitConfigurationProvider.ctor() from LibGit2Sharp.Configuration**
    - **refactor: redirect GitConfigurationProvider.ctor(path) to .ctor(LibGit2Sharp.Configuration)**

--- GitConfigurationProvider/GitConfigurationProvider.cs
+++ GitConfigurationProvider/GitConfigurationProvider.cs
@@ -10,14 +10,17 @@ public class GitConfigurationProvider : ConfigurationProvider, IDisposable
     readonly LibGit2Sharp.Configuration? configuration;
     readonly bool optional = true;
 
+    public GitConfigurationProvider(LibGit2Sharp.Configuration configuration, bool optional = true)
+    {
+        this.configuration = configuration;
+        this.optional = optional;
+    }
+
     public GitConfigurationProvider(bool optional = true)
         : this(path: Environment.CurrentDirectory, optional: optional) { }
 
     public GitConfigurationProvider(string path, bool optional = true)
-    {
-        this.configuration = LibGit2Sharp.Configuration.BuildFrom(Repository.Discover(path), null, null, null);
-        this.optional = optional;
-    }
+        : this(LibGit2Sharp.Configuration.BuildFrom(Repository.Discover(path), null, null, null), optional: optional) { }
 
     public override void Load()
     {
