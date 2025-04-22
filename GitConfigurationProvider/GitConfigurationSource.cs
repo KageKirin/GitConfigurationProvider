bb7d4a3b3cfb6aa674ea27759248dad1ca3d73cb
Christian Helmich <kagekirin@gmail.com>
2025-04-22 11:02:17 +0900

    feat: extend GitConfigurationProvider API with factory method taking repo/global/xdg/system configuration paths (feat/config-path) [#8]
    - **feat: implement GitConfigurationProvider.ctor() taking repo/global/xdg/system configuration paths**
    - **feat: implement GitConfigurationSource.ctor() taking repo/global/xdg/system configuration paths**
    - **feat: implement IConfigurationBuilder.AddGitConfig() extension method taking repo/global/xdg/system configuration paths**

--- GitConfigurationProvider/GitConfigurationSource.cs
+++ GitConfigurationProvider/GitConfigurationSource.cs
@@ -17,6 +17,24 @@ public class GitConfigurationSource : IConfigurationSource
         buildAction = () => new GitConfigurationProvider(path: path, optional: optional);
     }
 
+    public GitConfigurationSource(
+        string repositoryConfigurationPath,
+        string globalConfigurationPath,
+        string xdgConfigurationPath,
+        string systemConfigurationPath,
+        bool optional = true
+    )
+    {
+        buildAction = () =>
+            new GitConfigurationProvider(
+                repositoryConfigurationPath: repositoryConfigurationPath,
+                globalConfigurationPath: globalConfigurationPath,
+                xdgConfigurationPath: xdgConfigurationPath,
+                systemConfigurationPath: systemConfigurationPath,
+                optional: optional
+            );
+    }
+
     public IConfigurationProvider Build(IConfigurationBuilder builder)
     {
         return buildAction();
