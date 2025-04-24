d79dc296dd1b9593878a23a15e9760fcfe63bd8c
KageKirin <kagekirin@gmail.com>
2025-04-24 09:27:48 +0900

    refactor: dependency-inject the configuration data into the MainWindow instance

--- GitConfigurationViewer/MainWindow.axaml.cs
+++ GitConfigurationViewer/MainWindow.axaml.cs
@@ -1,11 +1,21 @@
+using System;
 using Avalonia.Controls;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace GitConfigurationViewer;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    public MainWindow(IConfiguration configuration, ILogger<MainWindow> logger)
     {
+        logger.LogTrace("MainWindow.ctor");
+
         InitializeComponent();
+        foreach (var config in configuration.GetSection("alias").GetChildren())
+        {
+            logger.LogTrace("{0}: {1}", config.Key, config.Value);
+        }
+        DataContext = configuration.GetSection("alias").GetChildren();
     }
 }
