79034f0dbaab0ffabecaef156f735438b91c5399
KageKirin <kagekirin@gmail.com>
2025-04-25 11:25:48 +0900

    refactor: dependency-inject MainWindowViewModel into the MainWindow instance
    and use as DataContext

--- GitConfigurationViewer/MainWindow.axaml.cs
+++ GitConfigurationViewer/MainWindow.axaml.cs
@@ -7,7 +7,7 @@ namespace GitConfigurationViewer;
 
 public partial class MainWindow : Window
 {
-    public MainWindow(IConfiguration configuration, ILogger<MainWindow> logger)
+    public MainWindow(IConfiguration configuration, MainWindowViewModel viewModel, ILogger<MainWindow> logger)
     {
         logger.LogTrace("MainWindow.ctor");
 
@@ -16,6 +16,7 @@ public partial class MainWindow : Window
         {
             logger.LogTrace("{0}: {1}", config.Key, config.Value);
         }
-        DataContext = configuration.GetSection("alias").GetChildren();
+
+        DataContext = viewModel;
     }
 }
