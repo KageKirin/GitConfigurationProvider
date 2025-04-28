28ebe18cf5a63c7e68f67fef55bdaa4948de5a77
KageKirin <kagekirin@gmail.com>
2025-04-28 19:15:26 +0900

    feat(ut): implement unit test for reading local pull settings through GitConfigurationProvider
    uses LibGit2Sharp.Configuration to set configuration values before test

--- Tests/ConfigurationTests/ConfigurationTests.cs
+++ Tests/ConfigurationTests/ConfigurationTests.cs
@@ -301,3 +301,47 @@ public class GlobalPullConfigurationTest : IClassFixture<TestRepositoryFixture>
         Assert.Equal("true", config.GetRequiredSection("pull")[autostashKey]);
     }
 }
+
+public class LocalPullConfigurationTest : IClassFixture<TestRepositoryFixture>
+{
+    private readonly TestRepositoryFixture fixture;
+    private const string rebaseKey = "rebase";
+    private const bool rebaseValue = true;
+    private const string autostashKey = "autostash";
+    private const bool autostashValue = true;
+
+    public LocalPullConfigurationTest(TestRepositoryFixture fixture)
+    {
+        this.fixture = fixture;
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.Repository);
+        Assert.NotNull(fixture.Repository.Config);
+
+        fixture.Repository.Config.ConfigLocalPull(key: rebaseKey, value: rebaseValue);
+        fixture.Repository.Config.ConfigLocalPull(key: autostashKey, value: autostashValue);
+    }
+
+    [Fact]
+    public void TestLib()
+    {
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.Repository);
+        Assert.NotNull(fixture.Repository.Config);
+
+        Assert.True(fixture.Repository.Config.Get<bool>($"pull.{rebaseKey}").Value);
+        Assert.True(fixture.Repository.Config.Get<bool>($"pull.{autostashKey}").Value);
+    }
+
+    [Fact]
+    public void TestConfig()
+    {
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.RepoDirectory);
+        Assert.NotEmpty(fixture.RepoDirectory);
+
+        IConfiguration config = new ConfigurationBuilder().AddGitConfig(path: fixture.RepoDirectory).Build();
+
+        Assert.Equal("true", config.GetRequiredSection("pull")[rebaseKey]);
+        Assert.Equal("true", config.GetRequiredSection("pull")[autostashKey]);
+    }
+}
