77c4313b833f77ecd34e807d5194dc1220cabd73
KageKirin <kagekirin@gmail.com>
2025-04-28 15:53:56 +0900

    feat(ut): implement unit test for injecting TempDirectoryFixture

--- Tests/ConfigurationTests/FixtureTests.cs
+++ Tests/ConfigurationTests/FixtureTests.cs
@@ -21,3 +21,21 @@ public class UsingTempDirectoryTest
         Assert.False(Path.Exists(path));
     }
 }
+
+public class TempDirectoryFixtureTest : IClassFixture<TempDirectoryFixture>
+{
+    private readonly TempDirectoryFixture fixture;
+
+    public TempDirectoryFixtureTest(TempDirectoryFixture fixture)
+    {
+        this.fixture = fixture;
+    }
+
+    [Fact]
+    public void Test()
+    {
+        Assert.True(true);
+        Console.WriteLine($"fixture.RepoDirectory: {fixture.RepoDirectory}");
+        Assert.True(Directory.Exists(fixture.RepoDirectory));
+    }
+}
