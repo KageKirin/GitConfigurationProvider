d540fc2166f0ae18ee2bd15dc44c73dac3fa866b
KageKirin <kagekirin@gmail.com>
2025-04-28 15:50:29 +0900

    feat(ut): implement disposable class TempDirectory to handle creation+deletion of a temp dir for testing

---
+++ Tests/ConfigurationTests/TestRepositoryFixture.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace ConfigurationTests;
+
+public class TempDirectory : IDisposable
+{
+    public readonly string RepoDirectory;
+
+    public TempDirectory(string? prefix = default)
+    {
+        RepoDirectory = Path.GetFullPath(Directory.CreateTempSubdirectory(prefix).FullName);
+        Assert.True(Directory.Exists(RepoDirectory));
+    }
+
+    ~TempDirectory()
+    {
+        Dispose(false);
+    }
+
+    public virtual void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        Directory.Delete(RepoDirectory, recursive: true);
+    }
+}
