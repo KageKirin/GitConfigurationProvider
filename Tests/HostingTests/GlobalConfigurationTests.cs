0548e65fd7eb5526fe81c8535d7b1a938f6dc84f
KageKirin <kagekirin@gmail.com>
2025-04-30 02:45:14 +0900

    feat(ut): implement unit test for reading global command alias through hosted and dependency-injected GitConfigurationProvider

--- Tests/HostingTests/GlobalConfigurationTests.cs
+++ Tests/HostingTests/GlobalConfigurationTests.cs
@@ -48,3 +48,43 @@ public class GlobalUserConfigurationTest : IClassFixture<HostingFixture>
         Assert.Equal(userEmail, config["user:email"]);
     }
 }
+
+public class GlobalAliasConfigurationTest : IClassFixture<HostingFixture>
+{
+    private readonly HostingFixture fixture;
+    private const string aliasName = "foobar";
+    private const string aliasCommand = "add --patch";
+
+    public GlobalAliasConfigurationTest(HostingFixture fixture)
+    {
+        this.fixture = fixture;
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.Repository);
+        Assert.NotNull(fixture.Repository.Config);
+
+        fixture.AddAlias(alias: aliasName, command: aliasCommand);
+    }
+
+    [Fact]
+    public void TestLib()
+    {
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.Repository);
+        Assert.NotNull(fixture.Repository.Config);
+
+        Assert.Equal(aliasCommand, fixture.Repository.Config.Get<string>($"alias.{aliasName}").Value);
+    }
+
+    [Fact]
+    public void TestConfig()
+    {
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.RepoDirectory);
+        Assert.NotEmpty(fixture.RepoDirectory);
+
+        using IHost host = fixture.CreateHost();
+        IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+        Assert.Equal(aliasCommand, config.GetRequiredSection("alias")[aliasName]);
+    }
+}
