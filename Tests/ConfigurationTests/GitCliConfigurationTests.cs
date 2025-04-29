c5e46a4e170b989303c6bf267619cfdf96be2548
KageKirin <kagekirin@gmail.com>
2025-04-29 15:27:58 +0900

    feat(ut): implement unit test for reading global logging log-level setting through GitConfigurationProvider
    uses git CLI wrapper to set configuration values before test

--- Tests/ConfigurationTests/GitCliConfigurationTests.cs
+++ Tests/ConfigurationTests/GitCliConfigurationTests.cs
@@ -465,3 +465,39 @@ public class CliGitLocalGearTokenConfigurationTest : IClassFixture<CliGitTestRep
         Assert.Equal(gearToken, config.GetRequiredSection("gears").GetRequiredSection(gearUrl.Replace(".", ":"))["token"]);
     }
 }
+
+public class CliGitGlobalLoggingLevelConfigurationTest : IClassFixture<CliGitTestRepositoryFixture>
+{
+    private readonly CliGitTestRepositoryFixture fixture;
+    private const string loggingSection = "Default";
+    private const string loggingLevel = "Trace";
+
+    public CliGitGlobalLoggingLevelConfigurationTest(CliGitTestRepositoryFixture fixture)
+    {
+        this.fixture = fixture;
+
+        CliGit.SetLogging(path: fixture.RepoDirectory, key: loggingSection, value: loggingLevel);
+    }
+
+    [Fact]
+    public void TestLib()
+    {
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.Repository);
+        Assert.NotNull(fixture.Repository.Config);
+
+        Assert.Equal(loggingLevel, fixture.Repository.Config.Get<string>($"Logging.LogLevel.{loggingSection}").Value);
+    }
+
+    [Fact]
+    public void TestConfig()
+    {
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.RepoDirectory);
+        Assert.NotEmpty(fixture.RepoDirectory);
+
+        IConfiguration config = new ConfigurationBuilder().AddGitConfig(path: fixture.RepoDirectory).Build();
+
+        Assert.Equal(loggingLevel, config.GetRequiredSection("Logging").GetRequiredSection("LogLevel")[loggingSection.Replace(".", ":")]);
+    }
+}
