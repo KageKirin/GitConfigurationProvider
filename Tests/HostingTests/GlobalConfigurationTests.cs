af2abb3d326c6a488eee4918882a2c45769b9730
KageKirin <kagekirin@gmail.com>
2025-04-30 02:43:37 +0900

    feat(ut): implement unit test for reading global user configuration through hosted and dependency-injected GitConfigurationProvider

---
+++ Tests/HostingTests/GlobalConfigurationTests.cs
@@ -0,0 +1,50 @@
+using KageKirin.Extensions.Configuration.GitConfig;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HostingTests;
+
+public class GlobalUserConfigurationTest : IClassFixture<HostingFixture>
+{
+    private readonly HostingFixture fixture;
+    private const string userName = "globalUser";
+    private const string userEmail = "global@localhost.com";
+
+    public GlobalUserConfigurationTest(HostingFixture fixture)
+    {
+        this.fixture = fixture;
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.Repository);
+        Assert.NotNull(fixture.Repository.Config);
+
+        fixture.ConfigUserName(userName: userName);
+        fixture.ConfigUserEmail(userEmail: userEmail);
+    }
+
+    [Fact]
+    public void TestLib()
+    {
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.Repository);
+        Assert.NotNull(fixture.Repository.Config);
+
+        Assert.Equal(userName, fixture.Repository.Config.Get<string>("user.name").Value);
+        Assert.Equal(userEmail, fixture.Repository.Config.Get<string>("user.email").Value);
+    }
+
+    [Fact]
+    public void TestConfig()
+    {
+        Assert.NotNull(fixture);
+        Assert.NotNull(fixture.RepoDirectory);
+        Assert.NotEmpty(fixture.RepoDirectory);
+
+        using IHost host = fixture.CreateHost();
+        IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+        Assert.Equal(userName, config["user:name"]);
+        Assert.Equal(userEmail, config["user:email"]);
+    }
+}
