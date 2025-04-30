28a3c232dd6f0bee54167a55e5acf328f798b7ac
KageKirin <kagekirin@gmail.com>
2025-04-30 13:18:18 +0900

    refactor(ut): change git CLI wrapper methods to actually set global settings
    caveat: this is potentially desctructive when run locally.

--- Tests/ConfigurationTests/CliGit.cs
+++ Tests/ConfigurationTests/CliGit.cs
@@ -32,9 +32,9 @@ public static class CliGit
 
     public static int Init(string path) => RunCommand($"git -C {path} init");
 
-    public static int ConfigUserName(string path, string userName) => RunCommand($"git -C {path} config user.name {userName}");
+    public static int ConfigUserName(string path, string userName) => RunCommand($"git -C {path} config --global user.name {userName}");
 
-    public static int ConfigUserEmail(string path, string userEmail) => RunCommand($"git -C {path} config user.email {userEmail}");
+    public static int ConfigUserEmail(string path, string userEmail) => RunCommand($"git -C {path} config --global user.email {userEmail}");
 
     public static int ConfigLocalUserName(string path, string userName) => RunCommand($"git -C {path} config --local user.name {userName}");
 
@@ -42,37 +42,37 @@ public static class CliGit
         RunCommand($"git -C {path} config --local user.email {userEmail}");
 
     public static int AddAlias(string path, string alias, string command) =>
-        RunCommand($"git -C {path} config alias.{alias} \"{command}\"");
+        RunCommand($"git -C {path} config --global alias.{alias} \"{command}\"");
 
     public static int AddLocalAlias(string path, string alias, string command) =>
         RunCommand($"git -C {path} config --local alias.{alias} \"{command}\"");
 
     public static int ConfigRebase(string path, string key, bool value) =>
-        RunCommand($"git -C {path} config rebase.{key} {value.ToString().ToLowerInvariant()}");
+        RunCommand($"git -C {path} config --global rebase.{key} {value.ToString().ToLowerInvariant()}");
 
     public static int ConfigLocalRebase(string path, string key, bool value) =>
         RunCommand($"git -C {path} config --local rebase.{key} {value.ToString().ToLowerInvariant()}");
 
     public static int ConfigPull(string path, string key, bool value) =>
-        RunCommand($"git -C {path} config pull.{key} {value.ToString().ToLowerInvariant()}");
+        RunCommand($"git -C {path} config --global pull.{key} {value.ToString().ToLowerInvariant()}");
 
     public static int ConfigLocalPull(string path, string key, bool value) =>
         RunCommand($"git -C {path} config --local pull.{key} {value.ToString().ToLowerInvariant()}");
 
     public static int ToggleRerere(string path, bool value) =>
-        RunCommand($"git -C {path} config rerere.enabled {value.ToString().ToLowerInvariant()}");
+        RunCommand($"git -C {path} config --global rerere.enabled {value.ToString().ToLowerInvariant()}");
 
     public static int ToggleLocalRerere(string path, bool value) =>
         RunCommand($"git -C {path} config --local rerere.enabled {value.ToString().ToLowerInvariant()}");
 
     public static int AddGearsToken(string path, string url, string token) =>
-        RunCommand($"git -C {path} config gears.\"{url}\".token {token}");
+        RunCommand($"git -C {path} config --global gears.\"{url}\".token {token}");
 
     public static int AddLocalGearsToken(string path, string url, string token) =>
         RunCommand($"git -C {path} config --local gears.\"{url}\".token {token}");
 
     public static int SetLogging(string path, string key, string value) =>
-        RunCommand($"git -C {path} config Logging.LogLevel.\"{key}\" {value}");
+        RunCommand($"git -C {path} config --global Logging.LogLevel.\"{key}\" {value}");
 
     public static int SetLocalLogging(string path, string key, string value) =>
         RunCommand($"git -C {path} config --local Logging.LogLevel.\"{key}\" {value}");
